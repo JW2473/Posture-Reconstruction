58286b25614d8ecc542cd05e38896892400416a6
jw <wuji13579@gmail.com>
2018-12-16 20:50:32 -0500

    fixed some bugs

--- python-code/processData.py
+++ python-code/processData.py
@@ -3,16 +3,17 @@
 #euler_order in oriTrakHAR: roll yaw pitch
 
 from scipy.interpolate import CubicSpline
-import tf
+import transformations
 import numpy as np
 import collections
 from pyquaternion import Quaternion
+threshold = 1
 
 def processRow(data_dirs, t):
-    torsoInterpo = createInterpolator(data_dir[0])
-    headInterpo = createInterpolator(data_dir[1])
-    leftInterpo = createInterpolator(data_dir[2])
-    rightInterpo = createInterpolator(data_dir[3])
+    torsoInterpo = createInterpolator(data_dirs[0])
+    headInterpo = createInterpolator(data_dirs[1])
+    leftInterpo = createInterpolator(data_dirs[2])
+    rightInterpo = createInterpolator(data_dirs[3])
 
     torsoQuats = torsoInterpo[6].evaluate(t)
     headQuats = headInterpo[6].evaluate(t)
@@ -24,59 +25,63 @@ def processRow(data_dirs, t):
     rightAccs = (rightInterpo[0].evaluate(t), rightInterpo[1].evaluate(t), rightInterpo[2].evaluate(t))
     
 
+    #torsoOffset = Quaternion([-0.01483, 0.659224, 0.747234, 0.082578])
+    #headOffset = Quaternion([-0.101687, 0.62569, 0.77248, 0.038392])
     torsoOffset = Quaternion([1, 0, 0, 0])
     headOffset = Quaternion([1, 0, 0, 0])
-    for i in range(0, t):
-        torsoQuat = torsoQuats.next()
+    for i in range(0, len(t)):
+        torsoQuat = next(torsoQuats)
         torsoQuatValid = torsoQuat[0] is not None and not (torsoQuat[0] == torsoQuat[1] and torsoQuat[0] == torsoQuat[2] and torsoQuat[0] == torsoQuat[3])
         if torsoQuatValid:
             correctedTorsoQuat = torsoQuat*torsoOffset
             torsoAcc = []
-            torsoAcc.append(torsoAccs[0].next())
-            torsoAcc.append(torsoAccs[1].next())
-            torsoAcc.append(torsoAccs[2].next())
+            torsoAcc.append(next(torsoAccs[0]))
+            torsoAcc.append(next(torsoAccs[1]))
+            torsoAcc.append(next(torsoAccs[2]))
             torsoAccMag = np.linalg.norm(torsoAcc)
             if torsoAccMag < threshold:
                 torsoOffset = Calibrate(torsoQuat)
         
-        headQuat = headQuats.next()
+        headQuat = next(headQuats)
         headQuatValid = headQuat[0] is not None and not (headQuat[0] == headQuat[1] and headQuat[0] == headQuat[2] and headQuat[0] == headQuat[3])
         if headQuatValid:
             correctedHeadQuat = headQuat*headOffset
             headAcc = []
-            headAcc.append(headAccs[0].next())
-            headAcc.append(headAccs[1].next())
-            headAcc.append(headAccs[2].next())
+            headAcc.append(next(headAccs[0]))
+            headAcc.append(next(headAccs[1]))
+            headAcc.append(next(headAccs[2]))
             headAccMag = np.linalg.norm(headAcc)
             if torsoQuatValid:
                 headRelativeQuat = correctedTorsoQuat.inverse*correctedHeadQuat
-                headRelative = tf.transformations.euler_from_quaternion(headRelativeQuat)
+                #headRelative = transformations.euler_from_quaternion(headRelativeQuat)
                 headAccRelative = [headAcc[i] - torsoAcc[i] for i in range(0, 3)]
                 if headAccMag < threshold:
                     headOffset = Calibrate(headQuat)
         
-        leftQuat = leftQuats.next()
+        leftQuat = next(leftQuats)
         leftQuatValid = leftQuat[0] is not None and not (leftQuat[0] == leftQuat[1] and leftQuat[0] == leftQuat[2] and leftQuat[0] == leftQuat[3])
         if leftQuatValid:
-            leftAcc.append(leftAccs[0].next())                
-            leftAcc.append(leftAccs[1].next())
-            leftAcc.append(leftAccs[2].next())
+            leftAcc = []
+            leftAcc.append(next(leftAccs[0]))                
+            leftAcc.append(next(leftAccs[1]))
+            leftAcc.append(next(leftAccs[2]))
             if torsoQuatValid:
                 leftRelativeQuat = correctedTorsoQuat.inverse*leftQuat
-                leftRelative = tf.transformations.euler_from_quaternion(leftRelativeQuat)
+                #leftRelative = transformations.euler_from_quaternion(leftRelativeQuat)
                 leftAccRelative = [leftAcc[i] - torsoAcc[i] for i in range(0, 3)]
 
-        rightQuat = rightQuats.next()
+        rightQuat = next(rightQuats)
         rightQuatValid = rightQuat[0] is not None and not (rightQuat[0] == rightQuat[1] and rightQuat[0] == rightQuat[2] and rightQuat[0] == rightQuat[3])
         if rightQuatValid:
-            rightAcc.append(rightAccs[0].next())
-            rightAcc.append(rightAccs[1].next())
-            rightAcc.append(rightAccs[2].next())
+            rightAcc = []
+            rightAcc.append(next(rightAccs[0]))
+            rightAcc.append(next(rightAccs[1]))
+            rightAcc.append(next(rightAccs[2]))
             if torsoQuatValid:
                 rightRelativeQuat = correctedTorsoQuat.inverse*rightQuat
-                rightRelative = tf.transformations.euler_from_quaternion(rightRelativeQuat)
+                #rightRelative = transformations.euler_from_quaternion(rightRelativeQuat)
                 rightAccRelative = [rightAcc[i] - torsoAcc[i] for i in range(0, 3)]
-        yield (headRelative, leftRelative, rightRelative, headAccRelative, leftAccRelative, rightAccRelative)
+        yield (headRelativeQuat, leftRelativeQuat, rightRelativeQuat, headAccRelative, leftAccRelative, rightAccRelative)
 
 
 
@@ -84,9 +89,11 @@ def readData(ind, data_dir):
     f = open(data_dir, 'r')
     line = f.readline()
     while line:
-        linelist = line.split(' ')
+        linelist = line.split(', ')
+        result = []
         for i in ind:
             result.append(float(linelist[i]))
+        line = f.readline()
         yield float(linelist[9]), result
 
 def createInterpolator(data_dir):
@@ -113,23 +120,20 @@ class InterpoQuat:
         self.data_time = data_time
 
     def evaluate(self, t):
-        time0, data0 = self.data_time.next()
-        time1, data1 = self.data_time.next()
+        t0, data0 = next(self.data_time)
+        t1, data1 = next(self.data_time)
         q = []
         for i in range(0, len(t)):
-            try:
-                while t[i] > time1:
-                    time0 = time1
-                    data0 = data1
-                    time1, data1 = self.data_time.next()
-            except e:
-                continue
-            q0 = tf.transformations.quaternion_from_euler(data0[0], data0[1], data0[2]) 
-            q1 = tf.transformations.quaternion_from_euler(data1[0], data1[1], data1[2])
+            while t[i] > t1:
+                t0 = t1
+                data0 = data1
+                t1, data1 = next(self.data_time)
+            q0 = transformations.quaternion_from_euler(data0[0], data0[1], data0[2]) 
+            q1 = transformations.quaternion_from_euler(data1[0], data1[1], data1[2])
             q0 = Quaternion(q0)
             q1 = Quaternion(q1)
 
-            w = (t[i] - t1)/(t2 - t1)
+            w = (t[i] - t0)/(t1 - t0)
             yield Quaternion.slerp(q0, q1, w)
 
 class InterpoCubic:
@@ -137,15 +141,15 @@ class InterpoCubic:
         self.data_time = data_time
 
     def evaluate(self, t):
-        qtime = collection.deque([])
-        qdata = collection.deque([])
+        qtime = collections.deque([])
+        qdata = collections.deque([])
         for i in range(0, 11):
-            time, data = next(data_time)
+            time, data = next(self.data_time)
             qtime.append(time)
             qdata.append(data)
         for i in range(0, len(t)):
             while t[i] > qtime[5]:
-                time, data = next(data_time)
+                time, data = next(self.data_time)
                 qtime.append(time)
                 qdata.append(data)
                 qtime.popleft()
@@ -155,11 +159,11 @@ class InterpoCubic:
 
 def Calibrate(quat):
     torsoZRotate = Quaternion([0.707, 0, 0, 0.707])
-    offset = quat*torso
-    euler = tf.transformations.euler_from_quaternion(offset)
+    offset = quat*torsoZRotate
+    euler = list(transformations.euler_from_quaternion([offset[0], offset[1], offset[2], offset[3]]))
     euler[0] = 0  #roll
     euler[1] = 0  #pitch
-    expected = tf.transformations.quaternion_from_euler(euler)
+    expected = transformations.quaternion_from_euler(euler[0], euler[1], euler[2])
     expected = Quaternion(expected)
     Offset =quat.inverse*expected
 
--- python-code/visualization.py
+++ python-code/visualization.py
@@ -1,34 +1,63 @@
 #euler_order: roll, pitch, yaw
 #original euler_order: yaw pitch roll
 #euler_order in oriTrakHAR: roll yaw pitch
+import numpy as np
 import math
 from pyquaternion import Quaternion
-import tf
+import transformations
 import json
+import matplotlib.pyplot as plt
+import processData
+from mpl_toolkits.mplot3d import Axes3D
 
 ANGLE_MAP_L = json.load(open('leftDict_yzx.json', 'r'))
 ANGLE_MAP_R = json.load(open('rightDict_yzx.json', 'r'))
-def updateRealtimeVis(quat, idStr):
+fig = plt.figure()
+ax = fig.gca(projection='3d')
+interpolatedData = processData.processRow(['test.csv', 'test2.csv','test3.csv','test4.csv'], [5.5])
+quatHead, quatLeft, quatRight, _, _, _ = next(interpolatedData)
+
+
+def updateRealtimeVis(quat, idStr, ax):
     if idStr == 'head':
+        headPos = quat.rotate([0, 0, 1])
+        ind = np.linspace(0,1,11)
+        x = [headPos[0]*i for i in ind]
+        y = [headPos[1]*i for i in ind]
+        z = [headPos[2]*i for i in ind]
+        ax.plot(x, y, z)
         return quat
-        #to be done
+
+    euler = tf.transformations.euler_from_quaternion([quat[0], quat[1], quat[2], quat[3]])
     if idStr == 'rightArm':
-        euler = tf.transformations.euler_from_quaternion(quat)
         ans =  ANGLE_MAP_R[rad2Bucket(euler[0])][rad2Bucket(euler[2])][rad2Bucket(euler[1])]
         if ans.shoulderX is not None:
             elbowRelativeEuler = [deg2rad(ans.shoulderX), deg2rad(ans.shoulderZ), deg2rad(ans.shoulderY)]
-            elbowRelativeQuat = tf.transformations.quaternion_from_euler(elbowRelativeEuler)
+            elbowRelativeQuat = transformations.quaternion_from_euler(elbowRelativeEuler[0], elbowRelativeEuler[1], elbowRelativeEuler[2])
             elbowRelativeQuat = Quaternion(elbowRelativeQuat)
+            elbowPos = elbowRelativeQuat.rotate([0, -1, -1])
+            wristRelativePos = quat.rotate([0, 0, -1])
+            ind = np.linspace(0,1,11)
+            x = [elbowPos[0]*i for i in ind] + [elbowPos[0]+wristRelativePos[0]*i for i in ind]
+            y = [elbowPos[1]*i for i in ind] + [elbowPos[1]+wristRelativePos[1]*i for i in ind]
+            z = [elbowPos[2]*i for i in ind] + [elbowPos[2]+wristRelativePos[2]*i for i in ind]
+            ax.plot(x, y, z)
             return elbowRelativeQuat
-        #to be done
+        
     if idStr == 'leftArm':
         ans =  ANGLE_MAP_L[rad2Bucket(euler[0])][rad2Bucket(euler[2])][rad2Bucket(euler[1])]
         if ans.shoulderX is not None:
             elbowRelativeEuler = [deg2rad(ans.shoulderX), deg2rad(ans.shoulderZ), deg2rad(ans.shoulderY)]
-            elbowRelativeQuat = tf.transformations.quaternion_from_euler(elbowRelativeEuler)
+            elbowRelativeQuat = transformations.quaternion_from_euler(elbowRelativeEuler[0], elbowRelativeEuler[1], elbowRelativeEuler[2])
             elbowRelativeQuat = Quaternion(elbowRelativeQuat)
+            elbowPos = elbowRelativeQuat.rotate([0, 1, -1])
+            wristPos = elbowPos + quat.rotate([0, 0, -1])
+            ind = np.linspace(0,1,11)
+            x = [elbowPos[0]*i for i in ind] + [elbowPos[0]+wristRelativePos[0]*i for i in ind]
+            y = [elbowPos[1]*i for i in ind] + [elbowPos[1]+wristRelativePos[1]*i for i in ind]
+            z = [elbowPos[2]*i for i in ind] + [elbowPos[2]+wristRelativePos[2]*i for i in ind]
+            ax.plot(x, y, z)
             return elbowRelativeQuat
-        #to be done
 
 def rad2Bucket(rad):
     radInterval = 5*3.1415926/180
@@ -37,3 +66,6 @@ def rad2Bucket(rad):
         return 180 - 5
     else:
         return ans
+
+updateRealtimeVis(quatHead, 'head', ax)
+plt.show()
